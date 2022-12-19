14bad05afe093c864f9fa1a3887ceec49759000c
shirei220 <110725093+shirei220@users.noreply.github.com>
2022-12-19 21:26:28 +1000

    expanded help description

--- commands/help.js
+++ commands/help.js
@@ -5,12 +5,10 @@ const path = require('node:path');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
-		.setDescription("displays list of sloop sloop bot's commands" 
+		.setDescription("displays list of sloop sloop bot's features" 
 		),
 	async execute(interaction) {
-		//dynamically gets names of commands + description
-		let help = "Hello! I can help you with the following commands:";
-
+		let help = "Hello! I can do the following:\n\n**Commands:**";
 		//gets directory of commands
 		interaction.client.commands = new Collection();
 		const commandsPath = path.join(__dirname);
@@ -29,6 +27,9 @@ module.exports = {
 				console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
 			}
 		}
+
+		help += "\n**Roles:**\nReact with emojis in the #roles channel to get the matching roles";
+		help += "\n**Other:**\n- factorial converter\n- s-word detector for a certain someone"
 		await interaction.reply({content: `${help}`, ephemeral: true});
 	},
 };
